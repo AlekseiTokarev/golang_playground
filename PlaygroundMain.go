3b9feba3d1aaa3141abb9c1be51362e036b80f45
Aleksei Tokarev <atokarev@hylamobile.com>
2021-02-15 15:34:36 -0600

    handle errors

--- PlaygroundMain.go
+++ PlaygroundMain.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(quote.Go())
-	fmt.Println(sum(
-		4,
-		7,
-	))
-	fmt.Println(sumAll([]int{5, 7, -78, 10}))
-	//
+
+	i := sum(4, 7)
+	fmt.Println(i)
+
+	all, err := sumAll([]int{5, 7, -78, 10})
+	fmt.Println(all)
+
+	all, err = sumAll([]int{})
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(all)
+
 	dto := new(Dto)
 	dto.Id = 4
 	dto.Name = "super"
@@ -23,15 +32,18 @@ func main() {
 
 }
 
-func sumAll(a []int) int {
+func sumAll(a []int) (n int, err error) {
 	var total int
+	if len(a) == 0 {
+		return 0, errors.New("array shouldn't be empty")
+	}
 	for _, v := range a {
 		if v < 0 {
 			continue
 		}
 		total += v
 	}
-	return total
+	return total, nil
 }
 
 func sum(a int, b int) int {
